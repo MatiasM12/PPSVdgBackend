32c2e75b5e8330eb81ff093cf7ff30035628aebf
Matias <86579814+MatiasM12@users.noreply.github.com>
2024-05-10 23:12:12 -0300

    Update CorsConfig.java

--- src/main/java/vdg/CorsConfig.java
+++ src/main/java/vdg/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 	    registry.addMapping("/**")
-	            .allowedOrigins("http://192.168.0.18:8100", "http://localhost:4200", "http://localhost:8100","https://vdgppsfrontend.onrender.com")
+	            .allowedOrigins("http://192.168.0.18:8100", "http://localhost:4200", "http://localhost:8100","https://vdgppsfrontend.onrender.com","https://d241-2800-af0-1448-51e0-909f-5785-3dd-3352.ngrok-free.app")
 	            .allowedMethods("GET", "POST", "PUT", "DELETE")
 	            .allowedHeaders("*")
 	            .exposedHeaders("Authorization")
